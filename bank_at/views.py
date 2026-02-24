cc558f070203d93c1fb4e75ac590ba3dc9016f87
Azamat Khamdamov <azamat_khamdamov@student.itpu.uz>
2026-02-24 17:34:50 +0500

    Fixed bug

--- bank_at/views.py
+++ bank_at/views.py
@@ -4,46 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import CreditInfoUpdateLog
-from bank_resurs_at.models import PaymentQueue, CreditData
 from config.permissions import IsResurs
-import threading
-import time
-
-
-worker_started = False
-def background_sender():
-    while True:
-        qs = PaymentQueue.objects.filter(sent=False).order_by("created_at")
-        items = []
-        for p in qs:
-            try:
-                credit = CreditData.objects.get(credit_id=p.credit_id)
-            except CreditData.DoesNotExist:
-                continue
-            items.append({
-                "credit_id": p.credit_id,
-                "paid_amount": p.paid_amount,
-                "credit_balance": credit.credit_balance,
-                "paid_percent_amount": credit.paid_precent_amount
-            })
-        if items:
-            CreditInfoUpdateLog.objects.create(
-                payload={"items": items},
-                status=200
-            )
-            qs.update(sent=True)
-        time.sleep(60)
 
 class CreditInfoUpdateAPIView(APIView):
-    permission_classes = [IsAuthenticated, IsResurs]
-
-    @swagger_auto_schema(operation_description="Online POST /credit-info-update/ metodi")
+    permission_classes = []
+    @swagger_auto_schema(operation_description="Online POST /credit-info-update/ metodi",)
     def post(self, request):
-        global worker_started
-        if not worker_started:
-            thread = threading.Thread(target=background_sender, daemon=True)
-            thread.start()
-            worker_started = True
         data = request.data
         CreditInfoUpdateLog.objects.create(
             payload=data,
--- bank_resurs_at/management/commands/push_updates.py
+++ bank_resurs_at/management/commands/push_updates.py
@@ -1,71 +1,54 @@
 import time
 import requests
+import os
 from django.core.management.base import BaseCommand
 from bank_resurs_at.models import PaymentQueue, CreditData
-import os
 
-BASE_URL = os.environ.get("BASE_URL")
-print(BASE_URL)
+BASE_URL = os.environ.get("BASE_URL", "https://bank-at.onrender.com")
 BANK_AT_URL = f"{BASE_URL}/api/v1/bank/credit-info-update/"
-TOKEN_URL   = f"{BASE_URL}/api/v1/token/"
-print(TOKEN_URL)
-USERNAME    = "bank_resurs_at"
-PASSWORD    = "Dj@ngo123"
-
-def get_jwt():
-    r = requests.post(TOKEN_URL, json={"username": USERNAME, "password": PASSWORD}, timeout=5)
-    r.raise_for_status()
-    return r.json()["access"]
 
 class Command(BaseCommand):
-    help = "Har 1 minutda paymentlarni yig‘ib bank_at ga yuboradi"
+    help = "Automatic push every 2 minutes"
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS("Update push loop started"))
-        access_token = get_jwt()
+        self.stdout.write("Push worker started")
+
         while True:
-            qs = PaymentQueue.objects.filter(sent=False).order_by("created_at")
-            items = []
-            for p in qs:
-                try:
-                    credit = CreditData.objects.get(credit_id=p.credit_id)
-                except CreditData.DoesNotExist:
-                    continue
-                items.append({
-                    "credit_id": p.credit_id,
-                    "paid_amount": p.paid_amount,
-                    "credit_balance": credit.credit_balance,
-                    "paid_percent_amount": credit.paid_precent_amount
-                })
-            if items:
+            try:
+                qs = PaymentQueue.objects.filter(sent=False)
+                items = []
+
+                for p in qs:
+                    try:
+                        credit = CreditData.objects.get(credit_id=p.credit_id)
+                    except:
+                        continue
+
+                    items.append({
+                        "credit_id": p.credit_id,
+                        "paid_amount": p.paid_amount,
+                        "credit_balance": credit.credit_balance,
+                        "paid_percent_amount": credit.paid_precent_amount
+                    })
+
                 payload = {
-                    "status": "PAYMENTS",
+                    "status": "PAYMENTS" if items else "NOPAYMENT",
                     "count": len(items),
                     "items": items
                 }
-            else:
-                payload = {
-                    "status": "NOPAYMENT",
-                    "message": "Ushbu oraliqda to'lov yo'q",
-                }
-            try:
+
                 r = requests.post(
                     BANK_AT_URL,
                     json=payload,
-                    headers={"Authorization": f"Bearer {access_token}"},
-                    timeout=40
+                    timeout=60  # muhim
                 )
-                if r.status_code == 401:
-                    access_token = get_jwt()
-                    r = requests.post(
-                        BANK_AT_URL,
-                        json=payload,
-                        headers={"Authorization": f"Bearer {access_token}"},
-                        timeout=40
-                    )
-                ok = r.status_code == 200
-            except Exception:
-                ok = False
-            if items and ok:
-                qs.update(sent=True)
-            time.sleep(120)
+
+                if r.status_code == 200 and items:
+                    qs.update(sent=True)
+
+                print("Push sent:", r.status_code)
+
+            except Exception as e:
+                print("Push error:", e)
+
+            time.sleep(120)  # 🔥 2 minut
